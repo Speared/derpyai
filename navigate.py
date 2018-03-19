192b000d3cdb82776c6eff17bb107c571ea1123b
David Speare <speare.david@gmail.com>
2018-03-18 23:00:51 -0400

    Pathfinding online!
    Less cool changes:
    Added shallow water and deep water to the map feature dict
    
    Considerable more cool changes
    Generating a path from one tile to another is in and seems to be working!

--- navigate.py
+++ navigate.py
@@ -19,6 +19,7 @@ MAP_WIDTH = 80
 MAP_HEIGHT = 70
 
 # key = rgb value, value = (name, glyph, a* pathfinding score)
+# Note that these glyphs arn't all accurate to the actual game
 FEATURE_NAME = 0
 FEATURE_GLYTH = 1
 FEATURE_ASCORE = 2
@@ -30,7 +31,9 @@ MAP_FEATURES = {
     (255, 0, 255): ("down stairway", ">", 1),
     (51, 51, 51): ("floor", ".", 1),
     (0, 0, 0): ("unexplored", " ", 10),
-    (255, 255, 255): ("player", "@", 0)
+    (255, 255, 255): ("player", "@", 1),
+    (17, 68, 85): ("shallow water", "~", 5),
+    (0, 17, 34): ("deep water", "`", 5)
     }
 
 minimap = None
@@ -64,35 +67,92 @@ def _get_squared_distance(pos1, pos2):
     return a_squared + b_squared
 
 
-def _get_a_node(coordinates):
+def _get_a_node(coordinates, parent):
     pixels = minimap.load()
     feature = MAP_FEATURES[pixels[coordinates]]
     travel_cost = feature[FEATURE_ASCORE]
-    return {'travel_cost': travel_cost, 'best_path': [], 'best_cost': [],
-            'open_neighbors': [], 'checked_neighbors': [],
-            'coordinates': coordinates}
+    # If travel cost is -1 this isn't a valid path tile
+    # Just return nothing
+    if travel_cost == -1:
+        return None
+    neighbors = _get_neighbors(coordinates)
+    new_node = {'travel_cost': travel_cost, 'best_path': [], 'best_cost': 0,
+            'open_neighbors': neighbors, 'coordinates': coordinates}
+    if parent != None:
+        best_path = list(parent['best_path'])
+        best_path.append(coordinates)
+        new_node['best_path'] = best_path
+        best_cost = parent['best_cost'] + travel_cost 
+        new_node['best_cost'] = best_cost
+        
+    #if parent != None:
+    #    print "from", parent['coordinates'], "to", coordinates
+    #    print ""
+    #    print "best path"
+    #    for p in new_node['best_path']:
+    #        print p
+    #    print ""
+    #    test = raw_input("Press Enter to continue...")
+        
+    return new_node
 
             
 def _get_path(start, goal):
-    startingNode = _get_a_node(start)
-    openNodes = [start]
-    checkedNodes = []
-    
+    startingNode = _get_a_node(start, None)
+    openNodes = [startingNode]
+    #key = coordinate, value = node
+    checkedNodes = {}
     while len(openNodes) > 0:
-        # First get the closest node to the goal
-        closest_node = None
-        closest_dist = 0
+        # First get the lowest-cost node
+        cheapest_node = None
+        cheapest_value = 0
         for node in openNodes:
-            if closest_node = None:
-                closest_node = node
-                closest_dist = _get_squared_distance(node[coordinates], goal)
+            if cheapest_node == None:
+                cheapest_node = node
+                cheapest_value = node['best_cost']
             else:
-                dist = _get_squared_distance(node[coordinates], goal)
+                if node['best_cost'] < cheapest_value:
+                    cheapest_value = node['best_cost']
+                    cheapest_node = node
+        # Now check each neighbor of this node, work with the closest open one
+        # No unchecked neighbors left means we can remove 
+        #   this from the open list
+        if len(cheapest_node['open_neighbors']) == 0:
+            openNodes.remove(cheapest_node)
+            continue
+        closest_neighbor = None
+        closest_dist = 0
+        for neighbor in cheapest_node['open_neighbors']:
+            if closest_neighbor == None:
+                closest_neighbor = neighbor
+                closest_dist = _get_squared_distance(neighbor[0], goal)
+            else:
+                dist = _get_squared_distance(neighbor[0], goal)
                 if dist < closest_dist:
+                    closest_neighbor = neighbor
                     closest_dist = dist
-                    closest_node = node
-        # Now check each neighbor of this node and add them to the path 
-                
+        # Pop this neighbor off the open stack to prevent re-checking
+        cheapest_node['open_neighbors'].remove(closest_neighbor)
+        new_node = _get_a_node(closest_neighbor[0], cheapest_node)
+        # Nodes will be none for invalid tiles (deep water, walls)
+        if new_node == None:
+            continue
+        # If our newest node is at the goal we're finished
+        if new_node['coordinates'] == goal:
+            return new_node['best_path']
+        # Check if this neighbor's coordinates are already in the dict
+        coordinates = new_node['coordinates']
+        if coordinates not in checkedNodes:
+            checkedNodes[coordinates] = new_node
+            openNodes.append(new_node)
+        else:
+            # If this node already exists replace it
+            #   if the new path is better
+            if new_node['best_cost'] < checkedNodes[coordinates]['best_cost']:
+                checkedNodes[coordinates] = new_node
+                openNodes.append(new_node)
+    # If we made it here we ran out of paths to try
+    return None            
             
     
 
@@ -127,6 +187,16 @@ def _get_map_frequencies():
         print rgb, '\t', frequency
 
 
+def _debug_print_path(path):
+    pixels = minimap.load()
+    print len(path)
+    for coordinate in path:
+        print coordinate
+        pixels[coordinate] = (255, 255, 0)
+    width, height = minimap.size
+    minimap.resize((width * 4, height * 4)).save('images/debug_path.bmp')
+    
+
 # Inject code into the web page, download the minimap canvas as
 #   a png and save it
 def _get_map(browser):
@@ -154,8 +224,31 @@ def _get_map(browser):
     minimap = minimap.resize((new_width, MAP_HEIGHT))
     print "new map height: {0} new map width: {1}".format(minimap.size[1],
                                                           minimap.size[0])
-    minimap.save('minimap.bmp')
-    
+
+
+def _tests(browser):
+    print "\nget map"
+    _get_map(browser)
+    print "\nsample frequencies"
+    _get_map_frequencies()
+    print "\nfind down staircases"
+    downstairs = _get_map_feature('>')
+    for stairs in downstairs:
+        print stairs
+    print "\nfind player"
+    player = _get_map_feature('@')[0]
+    print player
+    print "\nfind player's neighbors"
+    neighbors = _get_neighbors(player)
+    for neighbor in neighbors:
+        print neighbor
+    print "\nfind a path to a staircase"
+    path = _get_path(player, downstairs[0])
+    if path != None:
+        _debug_print_path(path)
+    else:
+        print "No path found"
+                                                          
 if __name__ == "__main__":
     # Open crawl, enter the game and try to get a map for test purposes
     # Copy/pasted from derpyai.py
@@ -195,19 +288,5 @@ if __name__ == "__main__":
                         (By.CLASS_NAME, 'game_message')))
     except selenium.common.exceptions.NoSuchElementException:
         pass
-
-    print "get map"
-    get_map(browser)
-    #print "sample frequencies"
-    #_get_map_frequencies()
-    print "find down staircases"
-    downstairs = _get_map_feature('>')
-    for stairs in downstairs:
-        print stairs
-    print "find player"
-    player = _get_map_feature('@')[0]
-    print player
-    print "find player's neighbors"
-    neighbors = _get_neighbors(player)
-    for neighbor in neighbors:
-        print neighbor
+    _tests(browser)
+    
